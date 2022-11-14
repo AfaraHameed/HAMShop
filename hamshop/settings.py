e40375e7c4b69ed9aaafa90ee1cfe08d8b256ecf
AfaraHameed <afarasidheek@gmail.com>
2022-11-14 15:19:09 +0530

    base.html added

--- hamshop/settings.py
+++ hamshop/settings.py
@@ -57,7 +57,7 @@ ROOT_URLCONF = 'hamshop.urls'
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [os.path.join(BASE_DIR,'templatesb')],
+        'DIRS': [os.path.join(BASE_DIR,'templates')],
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [
