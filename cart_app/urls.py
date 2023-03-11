6b9e9739c2298bb603d29a727aeb6b38db539a55
AfaraHameed <afarasidheek@gmail.com>
2023-03-11 17:56:06 +0530

    payment without successpage

--- cart_app/urls.py
+++ cart_app/urls.py
@@ -4,6 +4,7 @@ urlpatterns=[
     path('',views.cartdetails,name='cartdetails'),
     path('addcart/<int:product_id>/',views.add_cart,name='addcart'),
     path('cartdec/<int:product_id>/', views.min_cart, name='mincart'),
-    path('cartdel/<int:product_id>/', views.cart_delete, name='delcart')
+    path('cartdel/<int:product_id>/', views.cart_delete, name='delcart'),
+    path("callback/", views.callback, name="callback"),
 
 ]
--- cart_app/views.py
+++ cart_app/views.py
@@ -1,9 +1,14 @@
 from django.shortcuts import render,redirect,get_object_or_404
+from razorpay import Order
+
 from shop_app.models import *
 from cart_app.models import *
 from django.core.exceptions import ObjectDoesNotExist
 import razorpay
 from django.conf import settings
+from .constants import PaymentStatus
+from django.views.decorators.csrf import csrf_exempt
+import json
 
 # Create your views here.
 def cartdetails(request,tot=0,count=0,ct_items=None):
@@ -25,7 +30,17 @@ def cartdetails(request,tot=0,count=0,ct_items=None):
 
     print('*******')
     context={'ci':ct_items,'t':tot,'co':count,'payment':payment}
-    return  render(request,'cart.html',context)
+    return render(
+        request,
+        'cart.html',
+        {
+            "callback_url": "http://" + "127.0.0.1:8000" + "/callback/",
+            "razorpay_key": settings.KEY,
+            'ci': ct_items, 't': tot, 'co': count, 'payment': payment
+        },
+
+    )
+    #return  render(request,'cart.html',context)
 
 def c_id(request):
     ct_id = request.session.session_key
@@ -68,3 +83,36 @@ def cart_delete(request,product_id):
 # def checkout(request):
 #     client = razorpay.Client(auth = (settings.razor_pay_key_id , settings.key_secret))
 #     payment = client.order.create()
+
+@csrf_exempt
+def callback(request):
+    def verify_signature(response_data):
+        client = razorpay.Client(auth=(settings.KEY, settings.SECRET))
+        return client.utility.verify_payment_signature(response_data)
+
+    if "razorpay_signature" in request.POST:
+        payment_id = request.POST.get("razorpay_payment_id", "")
+        provider_order_id = request.POST.get("razorpay_order_id", "")
+        signature_id = request.POST.get("razorpay_signature", "")
+        order = Order.objects.get(provider_order_id=provider_order_id)
+        order.payment_id = payment_id
+        order.signature_id = signature_id
+        order.save()
+        if not verify_signature(request.POST):
+            order.status = PaymentStatus.SUCCESS
+            order.save()
+            return render(request, "callback.html", context={"status": order.status})
+        else:
+            order.status = PaymentStatus.FAILURE
+            order.save()
+            return render(request, "callback.html", context={"status": order.status})
+    else:
+        payment_id = json.loads(request.POST.get("error[metadata]")).get("payment_id")
+        provider_order_id = json.loads(request.POST.get("error[metadata]")).get(
+            "order_id"
+        )
+        order = Order.objects.get(provider_order_id=provider_order_id)
+        order.payment_id = payment_id
+        order.status = PaymentStatus.FAILURE
+        order.save()
+        return render(request, "callback.html", context={"status": order.status})
